9d8d1fa4781245195615b81191980757c24ff5d8
Osckar07 <50966035+Osckar07@users.noreply.github.com>
2019-11-12 10:49:18 -0600

    Corrección y arreglo de diseño antes del funcionamiento.

--- app/src/main/java/com/example/trivia_hn/MainActivity.kt
+++ app/src/main/java/com/example/trivia_hn/MainActivity.kt
@@ -4,15 +4,24 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         BtnAyuda.setOnClickListener {
-            val intent = Intent(this,Ayuda::class.java)
+            val intent = Intent(this, Ayuda::class.java)
+            startActivity(intent)
+            }
+
+        BtnModoFacil.setOnClickListener {
+            val intent = Intent(this, ModoFacil::class.java)
             startActivity(intent)
         }
+
+
     }
 }
---
+++ app/src/main/java/com/example/trivia_hn/ModoDificil.kt
@@ -0,0 +1,12 @@
+package com.example.trivia_hn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ModoDificil : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_modo_dificil)
+    }
+}
